667fda94d2f1b5586d7fa93515c41b98047b70fd
Ostap <gerasimovicostap@gmail.com>
2024-12-21 08:21:22 +0100

    Add project files.

---
+++ Repository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity item);
+        TEntity FindById(object id);
+
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate);
+
+        void Update(TEntity item);
+        void Remove(TEntity item);
+
+    }
+}
