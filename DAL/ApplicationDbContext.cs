f5d285bf9ae3356b5155053bad00868e08baaf52
Ostap <gerasimovicostap@gmail.com>
2024-12-21 09:00:26 +0100

    Created project DAL

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer("DATA SOURCE=NITRO\\SQLSERVEROSTAP; DATABASE=EFRepositoryProductCategory; UID=sa; PWD=1; TrustServerCertificate=True;");
+        }
+    }
+
+}
---
+++ DAL/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public int? CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+
+}
