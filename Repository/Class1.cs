1e3c0f011f27029b0f9596243a8e92d8da9c6029
Ostap <gerasimovicostap@gmail.com>
2024-12-21 08:38:56 +0100

    Added GenericUnitOfWork

---
+++ Repository/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class GenericUnitOfWork
+    {
+        DbContext context;
+
+        public GenericUnitOfWork(DbContext context)
+        { 
+            this.context = context;
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        
+        private Dictionary<Type, object> repositories = new Dictionary<Type, object>();
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class
+        {
+            if (repositories.Keys.Contains(typeof(TEntity)) == true)
+            {
+                return repositories[typeof(TEntity)] as IGenericRepository<TEntity>;
+            }
+
+            IGenericRepository<TEntity> repository = new EFGenericRepository<TEntity>(context);
+            repositories.Add(typeof(TEntity), repository);
+            return repository;
+
+        }
+
+
+
+    }
+}
